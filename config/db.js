d1630a24d31d1b3ab4a303e8e249715208fa6461
talhaDogar007 <talhadogar003@gmail.com>
2021-01-11 13:19:18 +0500

    initial

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log(`Database Connected to ${conn.connection.host}`.cyan.bold);
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const socket = require("socket.io");
+const morgan = require("morgan");
+const color = require("colors");
+const cors = require("cors");
+const connetDB = require("./config/db.js");
+const errorHandler = require("./middleware/errorhandler.js");
+dotenv.config({ path: "../backend/config/config.env" });
+
+const auth = require("./routes/auth.js");
+const { use } = require("./routes/auth.js");
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/v1/auth", auth);
+app.use(errorHandler);
+connetDB();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+var server = app.listen(
+  port,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT} `
+      .yellow.bold
+  )
+);
+process.on("unhandledRejection", (err, promisee) => {
+  console.log(`Error : ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
